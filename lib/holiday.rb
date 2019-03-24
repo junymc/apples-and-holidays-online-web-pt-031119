bc3ca3fe173fb5d73c29467a3bfa1c164717c811
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-24 06:38:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  supply_array = []
   supply_a = holiday_hash[:winter][:christmas]
   supply_b = holiday_hash[:winter][:new_years]
   binding.pry
